49407246850f6fc7314ea56846ce593067264c49
Henry Pan <henrypoon1231@gmail.com>
2017-05-21 09:56:24 +1000

    add view original

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
  	has_many :pins
- 	has_many :comments, dependent: :destroy
 end
