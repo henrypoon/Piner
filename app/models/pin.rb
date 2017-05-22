7425add6a5bd1a238128e766a12bfe0fa522835b
Henry Pan <henrypoon1231@gmail.com>
2017-05-22 16:23:45 +1000

    fix destroy

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,5 +3,5 @@ class Pin < ApplicationRecord
 	belongs_to :user
 	has_attached_file :image, styles: { medium: "300x300>"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
-	has_many :comments
+	has_many :comments, dependent: :destroy
 end
